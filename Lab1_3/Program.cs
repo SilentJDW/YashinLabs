e9c90f21e2b0b6ec228f866ed73e1589b66419f0
SilentJDW <kilanawa@gmail.com>
2023-12-12 16:58:49 +0300

    Выполнен второй день

---
+++ Lab1_3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lab1_3
+{
+    record class SyncObj(double n);
+
+    public class MyObject
+    {
+        public double n;
+        public bool waitCos = false;
+        public void Cos()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (this)
+                {
+                    while (waitCos) { Monitor.Wait(this); }
+                    n = Math.Cos(n);
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.Name}, value: {n}");
+                    waitCos = true;
+                    Monitor.Pulse(this);
+
+                }
+            }
+        }
+
+        public void ArkCos()
+        {
+            for (int a = 0; a < 10; a++)
+            {
+                lock (this)
+                {
+                    while (!waitCos) { Monitor.Wait(this); }
+                    n = Math.Acos(n);
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.Name}, value: {n}");
+                    waitCos = false;
+                    Monitor.Pulse(this);
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyObject sync = new MyObject() { n = 1.0 };
+            Thread t1 = new Thread(sync.Cos) { Name = "A" };
+            Thread t2 = new Thread(sync.ArkCos) { Name = "B" };
+
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
---
+++ Lab1_4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Lab1_4
+{
+    internal class Program
+
+    {
+        public static void Recieve(int sreq, ref Socket sclient)
+        {
+            Console.WriteLine($"Remote client: {sclient.RemoteEndPoint}");
+            using var stream = new NetworkStream(sclient);
+
+            using var r = new StreamReader(stream);
+            using var w = new StreamWriter(stream);
+
+            string result = r.ReadLine();
+            Console.WriteLine($"Received: {result}, Requests: {sreq}");
+            //Thread.Sleep(100); //
+
+            w.WriteLine(result.ToUpper());
+            w.Flush();
+            sclient.Dispose();
+
+        }
+        static void Main(string[] args)
+        {
+            const int PORT = 1111;
+            const int MAX_QUEUE = 10;
+            Console.WriteLine("Hello, World!");
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, PORT);
+            using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipPoint);
+            socket.Listen(MAX_QUEUE);
+            int req = 0;
+            object sync = new object();
+            while (true)
+            {
+                Socket client = socket.Accept();
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        Console.WriteLine($"Remote client: {client.RemoteEndPoint}");
+                        using var stream = new NetworkStream(client);
+
+                        using var r = new StreamReader(stream);
+                        using var w = new StreamWriter(stream);
+                        lock (sync) req++;
+                        string result = r.ReadLine();
+                        Console.WriteLine($"Received: {result}, Requests: {req}");
+                        //Thread.Sleep(100); //
+
+                        w.WriteLine(result.ToUpper());
+                        w.Flush();
+                    }
+                    finally
+                    {
+                        client.Dispose();
+                    } 
+                });
+            }
+        }
+    }
+}
